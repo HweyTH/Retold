4766a1f85090d9537c2faf7c54b33a7b708592e8
Jonathan Yu <jonathany.yu@mail.utoronto.ca>
2024-05-29 00:41:10 -0400

    Added a search function - Trying it out.

---
+++ tests/search.py
@@ -0,0 +1,37 @@
+"""
+Retold
+
+Functions for searching MangaDex's API.
+"""
+
+
+import requests
+
+
+URL = "https://api.mangadex.org"
+
+
+def search_title(title: str, limit: int, page: int) -> list[dict]:
+    """
+    Search for manga.
+    :param title: The title of the manga to be searched for.
+    :param limit: The maximum number of results that are returned per page.
+                    limit >= 1
+    :param page: The page of results from
+                    [(page - 1) * limit] to [page * limit]. page >= 1
+    :return: dict - the data from the json-encoded content of the response.
+                    MangaDex represents this data as a list of dictionary
+                    objects.
+    """
+    if title == "":
+        raise Exception("Invalid title.")
+    if limit < 1:
+        raise Exception("Invalid limit.")
+    if page < 1:
+        raise Exception("Invalid page.")
+
+    params = {"title": title, "limit": limit, "offset": limit * (page - 1)}
+    request = requests.get(f"{URL}/manga", params=params)
+    result = request.json()["data"]
+
+    return result
---
+++ tests/search_tests.py
@@ -0,0 +1,9 @@
+"""
+Retold
+"""
+
+
+from search import search_title
+
+
+print(search_title("", 1, 1))
