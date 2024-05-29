5e4da08336f24ea56af9d45c8ca301c403d2532c
Jonathan Yu <jonathany.yu@mail.utoronto.ca>
2024-05-29 00:43:33 -0400

    Fixed some formatting.

--- tests/search.py
+++ tests/search.py
@@ -16,12 +16,11 @@ def search_title(title: str, limit: int, page: int) -> list[dict]:
     Search for manga.
     :param title: The title of the manga to be searched for.
     :param limit: The maximum number of results that are returned per page.
-                    limit >= 1
+        limit >= 1
     :param page: The page of results from
-                    [(page - 1) * limit] to [page * limit]. page >= 1
-    :return: The data from the json-encoded content of the response.
-                    MangaDex represents this data as a list of dictionary
-                    objects.
+        [(page - 1) * limit] to [page * limit]. page >= 1
+    :return: The data from the json-encoded content of the response. MangaDex
+        represents this data as a list of dictionary objects.
     """
     if title == "":
         raise Exception("Invalid title.")
